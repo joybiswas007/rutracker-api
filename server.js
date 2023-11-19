5e74fe155339e4ea03e9db61b3acf8e1ad270a8d
Joy Biswas <joybiswas040701@gmail.com>
2023-11-19 17:39:08 +0600

    enabled cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 10000;
 app.use(express.json());
+app.use(cors());
 
 //Import routes
 const ruTracker = require("./api/routes/rutracker");
