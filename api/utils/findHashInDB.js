c55e27ffa1902694df9acda75067f9b8636d39e6
Joy Biswas <joybiswas040701@gmail.com>
2024-02-06 10:06:22 +0600

    small fix

--- api/utils/findHashInDB.js
+++ api/utils/findHashInDB.js
@@ -1,7 +1,9 @@
 const { Search } = require("../db/dbSchema");
 
 const findHashInDB = async (hash) => {
-  const search = await Search.findOne({ infohash: hash });
+  const search = await Search.findOne({ infohash: hash }).select(
+    "_id filename filesize seeders leechers snatches magnet infohash uploadedDate topic downloadId"
+  );
   return search;
 };
 
